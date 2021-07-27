ed63cf2cfc41b39baf939c2708a4d2cf3737ab22
Juan Vargas <vargasjuan91@gmail.com>
2021-07-27 17:43:39 -0500

    Init Practical Disney Word

---
+++ Technical-Challenges/Spring-Data-Security/src/main/java/co/com/practical/disney_word/DisneyWordApplication.java
@@ -0,0 +1,13 @@
+package co.com.practical.disney_word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyWordApplication.class, args);
+	}
+
+}
---
+++ Technical-Challenges/Spring-Data-Security/src/test/java/co/com/practical/disney_word/DisneyWordApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.practical.disney_word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
