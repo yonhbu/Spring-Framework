f129dce9d087a9d5f8f9722e4b22376632681e37
Juan Vargas <vargasjuan91@gmail.com>
2021-10-20 15:24:07 -0500

    Faltan las Pruebas Unitarias, Clean A. Sin Relaciones de Entidades

--- Clean_Architecture_Software/Rest-Event-Services/infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/event/jpa/util/ObjectMapperUtils.java
+++ Clean_Architecture_Software/Rest-Event-Services/infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/event/jpa/util/ObjectMapperUtils.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.data.mapping.context.MappingContext;
 
 public class ObjectMapperUtils {
 
